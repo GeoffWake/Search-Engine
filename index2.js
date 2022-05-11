8f752f59c792f5fe79e99212c8db5263cad7f0c6
Geoffrey Wakeford <you@example.com>
2022-05-11 16:27:02 +1000

    Third Commit

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 const bookURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
-
+gridpostions = ["grid1", "grid2", "grid3", "grid4", "grid5", "grid6", "grid7", "grid8", "grid9", "grid10",]
 ///add an event listener to submit button
 const button = document.getElementById("submit")
 const box= document.getElementById("searchBox")
@@ -29,16 +29,19 @@ const search = async(searchTerm) => {
     const response = await promiseRequest 
     const searchData = await response.json()
     
-  // console.log(searchData)
+   console.log(searchData)
 
    ///Book details array
 const results= searchData.items.map((volume) => {
     const output= {}
     output.title = volume.volumeInfo.title
+    //output.subtitle = volume.volumeInfo.subtitle
+    
     //output.author=volume.volumeInfo.author
-    output.description= volume.volumeInfo.description
-
-    console.log(output)//Out
+ //output.description= volume.volumeInfo.description
+    output.image = volume.volumeInfo.imageLinks;
+    
+   //console.log(output.image.thumbnail)
 
     //firstbox information
     
@@ -51,15 +54,41 @@ const results= searchData.items.map((volume) => {
 
 
 })
-const displayOne =document.getElementById("grid1")
-console.log(results) ///Result of function 
-const info=document.createTextNode(results[0])
-    displayOne.appendChild(info)
+//const displayOne =document.getElementById("grid1")
+//console.log(results[0].title) ///Result of function 
+//const info=document.createTextNode(results[i].title)
+ //   displayOne.appendChild(info//
+
+    for (let i = 0; i < gridpostions.length; i++) {
+        const fred = document.getElementById(gridpostions[i]);
+
+       const title=document.createTextNode(results[i].title)
+       //const info2=document.createTextNode(results[i].description)
+
+       ///Display Image
+       const img = document.createElement("img")
+      img.src= results[i].image.thumbnail;
+       console.log(results[i].description)
+       //const info3=document.createTextNode( results[i].image.thumbnail)
+       ///( results[i].image.thumbnail)
+        //console.log(info3)
+        //let img =document.createElement("img");
+       
+        
 
 
-//console.log(results)
 
+
+        ///Fred this appends to Page
+
+        fred.appendChild(img)
+
+        
+       fred.appendChild(title)
+        //fred.appendChild(info2)
+       
+
+
+    }
 }
 
-
-    
